b254c2d478a9ed4f6ff068745acce284a88fe1f8
Zen <zenblackswan@gmail.com>
2023-01-05 11:44:15 +0300

    Edit example ping_pong

---
+++ examples/ping_pong/README.md
@@ -0,0 +1,19 @@
+# Ping_pong experiment
+
+Models of the agent type are connected in a ring (the output of the previous model is connected to the input of the next model). Some models have an initial state of **STRIKE**, others have an initial state of WAITING. During an internal transition, models that are in the **STRIKE** state emit a message ("ball") with the number of the strike made on this "ball". During an external transition, models that are in the **WAITING** state go into the STRIKE state for a random time interval from 2 to 10. The Observer writes the history of its transitions and states to the files of each model.
+
+To run the model, enter:
+
+'''rust
+    cargo run --example ping_pong -- {mode} {path/to/experiment}
+'''
+
+Where:
+mode - `single` for run in single thread, `milti` for run in multithred
+path/to/experiment - relative or absolute path to experiment config file (JSON)
+
+Example
+
+'''rust
+    cargo run --example ping_pong -- multi experiment examples/ping_pong/experiment_1.json
+'''
--- examples/ping_pong/main.rs
+++ examples/ping_pong/main.rs
@@ -1,4 +1,7 @@
-use std::env;
+use std::{
+    env,
+    path::{Path, PathBuf},
+};
 
 extern crate exdsdevs;
 use exdsdevs::{
@@ -12,33 +15,51 @@ use ping_pong::{AgentDynamic, RootDynamic};
 pub mod ping_pong;
 
 pub fn main() {
-    match env::args().nth(1) {
-        Some(mode) if mode.as_str() == "single" => {
-            println!("Running single thread");
-            create_experiment().run_single_thread();
-        }
-        Some(mode) if mode.as_str() == "multi" => {
-            println!("Running multi thread");
-            create_experiment().run_multi_thread();
-        }
+    let mode = match env::args().nth(1) {
+        Some(mode) if mode.as_str() == "single" => mode,
+        Some(mode) if mode.as_str() == "multi" => mode,
         _ => {
-            println!("ERROR: enter the parameter: `single` - for run in single thread, `multi` - for run in multi thread");
+            panic!("ERROR: enter the parameter: `single` - for run in single thread, `multi` - for run in multi thread");
         }
+    };
+
+    let experiment_path = match env::args().nth(2) {
+        Some(path_string) => {
+            let path = PathBuf::from(&path_string);
+            let exp_path = if !path.is_absolute() {
+                let mut path = env::current_dir().unwrap();
+                path.push(&path_string);
+                path
+            } else {
+                path
+            };
+            if exp_path.exists() {
+                exp_path
+            } else {
+                panic!("Path {} does not exist", &exp_path.to_string_lossy());
+            }
+        }
+        None => panic!("ERROR: enter the path to experiment config"),
+    };
+    let mut experiment = create_experiment(&experiment_path);
+    match mode.as_str() {
+        "single" => {
+            println!("Running single thread");
+            experiment.run_single_thread();
+        }
+        "multi" => {
+            println!("Running multi thread");
+            experiment.run_multi_thread();
+        }
+        _ => {}
     }
 }
 
-fn create_experiment() -> Experiment {
+fn create_experiment(experiment_path: &Path) -> Experiment {
     let dynamic_factory = DynamicFactoryStorage::new()
         .with_dynamic_factory("root", DynamicFactory::<RootDynamic>::new())
         .with_dynamic_factory("agent", DynamicFactory::<AgentDynamic>::new());
     let observer_factory = ObserverFactoryStorage::new()
         .with_observer_factory("std_logger", ObserverFactory::<Logger>::new());
-    let experiment_path = env::current_dir()
-        .map(|mut ep| {
-            ep.push("examples/ping_pong/experiment_1.json");
-            ep
-        })
-        .unwrap()
-        .to_owned();
     Experiment::new(&experiment_path, dynamic_factory, observer_factory)
 }
